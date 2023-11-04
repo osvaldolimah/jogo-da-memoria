75a5b5ec033435e27e4548d4596fff85c3de478c
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-03 22:22:19 -0300

    criado o array com emojis e as primeiras funcoes

---
+++ src/scripts/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+    "🤢",
+    "🤢",
+    "😨",
+    "😨",
+    "🥵",
+    "🥵",
+    "🥶",
+    "🥶",
+    "😍",
+    "😍",
+    "😈",
+    "😈",
+    "🙉",
+    "🙉",
+    "🤡",
+    "🤡"
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement ("div"); 
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
